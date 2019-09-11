0ea3d6dde3c91752cb99ed269a0ae75fef26502e
josehenriquelicio <jose.licio@b2wdigital.com>
2019-09-11 17:26:32 -0300

    fix: muda o nome da exchange

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ const config = {
     rabbit: {
         host: RABBITMQ_HOST || 'rabbit',
         port: RABBITMQ_PORT || '5672',
-        default_queue: RABBITMQ_DEFAULT_QUEUE || 'fluentd',
-        default_exchange: RABBITMQ_DEFAULT_EXCHANGE || 'fluentd'
+        default_queue: RABBITMQ_DEFAULT_QUEUE || 'test',
+        default_exchange: RABBITMQ_DEFAULT_EXCHANGE || 'test'
     }
 };
 
